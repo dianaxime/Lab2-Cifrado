fbb738ea83124d6d030530febaa70fc1e687f9ad
iconicmajo <42973036+iconicmajo@users.noreply.github.com>
2020-07-31 21:01:58 -0600

    Update decript.py

--- decript.py
+++ decript.py
@@ -7,6 +7,7 @@
 
 #Laboratorio 2
 #BLOCK CIPHERS
+#se esta trabajando con un archivo .TXT
 
 '''
 CODIGO REFERNECIADO DE:
@@ -19,9 +20,9 @@ import os
 
 from Crypto.Cipher import AES
 
-IV_SIZE = 16    # 128 bit, fixed for the AES algorithm
-KEY_SIZE = 32   # 256 bit meaning AES-256, can also be 128 or 192 bits
-SALT_SIZE = 16  # This size is arbitrary
+IV_SIZE = 16    # Largo de la cadena de 128 bits, estandarizada para el algoritmo AES
+KEY_SIZE = 32   # Largo de la llave de 256 bit modificado para el algoritmo AES-256 (32*8=256)
+SALT_SIZE = 16  # Tamano recomendado para esta encripccion
 
 #aqui vamos a leer el archivo con el texto encriptado
 encriptado = open ('cript.txt','rb')
@@ -32,11 +33,16 @@ password = input('Ingrese su password segura')
 #convertir de texto a bytes
 password= str.encode(password)
 
+#accdede al tamano de la sal, y ayuda a encontrar la clave
 salt = encrypted[0:SALT_SIZE]
+#se genera el vector de inicializacion y la clave del cifrado
 derived = hashlib.pbkdf2_hmac('sha256', password, salt, 100000,
                               dklen=IV_SIZE + KEY_SIZE)
+#vector inicial con tamano 16
 iv = derived[0:IV_SIZE]
+#llave con tamano 32
 key = derived[IV_SIZE:]
+
 #aqui se desencripta el texto
 cleartext = AES.new(key, AES.MODE_CFB, iv).decrypt(encrypted[SALT_SIZE:])
 #Escribir el archivo con el texto desencriptado
